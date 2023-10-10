e72f807b0f8f993f9e632d82f0b73cb45777277e
crackc0der <natsaev1989@gmail.com>
2023-10-10 17:36:22 +0300

    deleted comments

--- main.go
+++ main.go
@@ -9,18 +9,6 @@ import (
 )
 
 func main() {
-	// if os.Args[1] == "s" {
-	// 	m := Mask{mask: '*', space: " ", prefix: "http://"}
-	// 	str := m.DisguiseStr("1 http://11111111111 2 http://22222222222 http://sdfaiojdsfoi http://sd8923892348923")
-	// 	//nolint:forbidigo
-	// 	fmt.Println(str)
-	// } else if os.Args[1] == "f" {
-	// 	m := Mask{mask: '*', space: " ", prefix: "http://"}
-	// 	err := m.DisguiseFile("links.txt", "disguised_links.txt")
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
 	app := &cli.App{
 		Commands: []cli.Command{
 			{
